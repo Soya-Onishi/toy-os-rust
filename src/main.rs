ed1be073392e13965dbed8cf651a9981b2e8431c
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-12-10 11:41:35 +0900

    GDBによるデバッグをするかどうかを決定する"--gdb"オプションを追加

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+  #[arg(long, default_value_t = false)]
+  gdb: bool
+}
+
 fn main() {
+    let args = Args::parse();
+
     // read env variables that were set in build script
     let bios_path = env!("BIOS_PATH");
     println!("{}", &bios_path);
@@ -6,9 +16,12 @@ fn main() {
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
     cmd.arg("-drive").arg(format!("format=raw,file={bios_path}"));
     cmd.args(["-monitor", "stdio"]);
-    cmd.args(["-gdb", "tcp::9000"]);
-    cmd.arg("-S");
 
+    if args.gdb {
+      cmd.args(["-gdb", "tcp::9000"]);
+      cmd.arg("-S");
+    }
+    
     let mut child = cmd.spawn().unwrap();
     child.wait().unwrap();
 }
